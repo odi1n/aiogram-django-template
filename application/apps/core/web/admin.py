04c42d2c9cd2b4dfec038337a68dcb6339963275
odi1n <sashf1999@mail.ru>
2021-08-14 23:43:05 +0300

    Update model UserAdmin

--- application/apps/core/web/admin.py
+++ application/apps/core/web/admin.py
@@ -3,10 +3,13 @@ from orm_converter import TortoiseToDjango
 
 from .. import models
 
+
 # Register your models here.
 # To convert models: TortoiseToDjango.convert(models.ModelName)
 
 
 @admin.register(TortoiseToDjango.convert(models.User))
 class UserAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['tg_id',
+                    'chat_id',
+                    'first_name']
