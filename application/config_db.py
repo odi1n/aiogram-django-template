73c818713c0e31a06fc974923e6ca01589dde427
odi1n <sashf1999@mail.ru>
2021-08-15 12:46:55 +0300

    I moved the settings to a higher category

---
+++ application/config_db.py
@@ -0,0 +1,12 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_API_TOKEN = os.environ.get("BOT_API_TOKEN")
+
+DB_HOST = os.environ.get("DB_HOST")
+DB_PORT = os.environ.get("DB_PORT")
+DB_NAME = os.environ.get("DB_NAME")
+DB_USER = os.environ.get("DB_USER")
+DB_PASSWORD = os.environ.get("DB_PASSWORD")
